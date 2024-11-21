6cec30d21db19195e0f4d2d8507a8a8e2683b352
lzh20201997 <1500891738@qq.com>
2024-11-22 01:24:12 +0800

    test: 测试自动部署

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <div>
         <h1>test</h1>
+        <h2>第二次修改</h2>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
